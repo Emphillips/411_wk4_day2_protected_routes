4efe9c8e6ebb51874017076feb95a01f46000746
emphillips <matt-phillips@live.com>
2020-02-07 16:08:39 -0600

    Updated Router and Login pages with Protected routes and cookie info

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,8 +20,10 @@ class App extends Component {
 
   login = (e) => {
     e.preventDefault()
-    // set cookie here
+    document.cookie = "loggedIn=true;max-age=60*1000"
+    
     // set loggedIn = true and max-age = 60*1000 (one minute)
+    
 
     window.location.replace("/")
   }
